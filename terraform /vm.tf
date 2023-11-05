6d6e565046aa0254c529b292c6a8c2ac7ddb9f78
lifeofpratik <lifeofpratik17@gmail.com>
2023-11-05 23:32:26 +0530

    Update config

--- terraform /vm.tf
+++ terraform /vm.tf
@@ -10,13 +10,14 @@ resource "azurerm_resource_group" "test_rg" {
 }
 
 resource "azurerm_linux_virtual_machine" "test_vm" {
-  name                = "app-test-vm"
-  resource_group_name = azurerm_resource_group.test_rg.name
-  location            = azurerm_resource_group.test_rg.location
-  size                = "Standard_F2"
-  admin_username      = "adminuser"
-  admin_password      = random_password.password.result
-  custom_data         = base64encode(file("${path.module}/assets/script.sh"))
+  name                            = "app-test-vm"
+  resource_group_name             = azurerm_resource_group.test_rg.name
+  location                        = azurerm_resource_group.test_rg.location
+  size                            = "Standard_B2s"
+  admin_username                  = "devopsuser"
+  disable_password_authentication = false
+  admin_password                  = random_password.password.result
+  custom_data                     = base64encode(file("${path.module}/assets/script.sh"))
   network_interface_ids = [
     azurerm_network_interface.test_nic.id,
   ]
